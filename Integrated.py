9d840b8dcfd175b15e422fddf79cc8f6b18de6ea
Sudarshan Samal <sudarshan.samal64@gmail.com>
2016-12-20 20:29:02 +0530

    Update Integrated.py
    There was a if __init__ check... did it by mistake

--- Integrated.py
+++ Integrated.py
@@ -31,10 +31,6 @@ def do_obstacle(board, tmap):
     return tmap
 
 def do_path(board, tmap):
-    if __name__ == '__main__':
-        for block in board :
-            if block[0:2] != ('black', '4-sided'):
-                # ok fuck... we dont have the xB and yB coordinates here, we need to find which is the shortest destination.
-                # ok... we can get that after mapping to all the destination and comapring to find the shortest one
-                # so in that find_path function in AstarAlgo, do something!!!... m sleeping.z.z.z.zzzz
-                pass
+    for block in board :
+        if block[0:2] != ('black', '4-sided'):
+               
